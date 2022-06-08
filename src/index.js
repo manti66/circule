341e3e401deb5026e40531b0fa6e1e8dcffca0a2
manti66 <bahura911@abv.bg>
2022-06-08 22:56:05 +0300

    Add css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 
 
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
+app.use('/static', express.static('public'))
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine','hbs')
+app.set('views', './src/views')
+
 app.get('/', (req, res) => {
-    res.send('Helloo')
+    res.render('index')
 })
 
 app.listen(5000, () => console.log('Finisshhhhh'))
