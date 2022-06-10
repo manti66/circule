c2e883c5440b4bf1efa6f9417a398f2537df8da4
manti66 <bahura911@abv.bg>
2022-06-10 16:31:52 +0300

    Add create and about

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,13 @@
 const express = require('express');
 const homeController = require('./controllers/homeController');
 
-
+const cubeController = require('./controllers/cubeController')
 const router = express.Router();
 
 router.get('/',  homeController.index);
+router.get('/about', (req, res)=>{
+    res.render('about')
+})
+router.use('/cube', cubeController)
 
 module.exports = router;
