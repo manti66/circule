8b8b40249df28def020a1034c693b9efcf88e980
manti66 <bahura911@abv.bg>
2022-06-12 16:14:08 +0300

    Add Servic Folder

---
+++ src/service/cubeService.js
@@ -0,0 +1,10 @@
+
+const cubes = require('../db.json')
+const fs = require('fs/promises');
+const path = require('path');
+
+exports.save =(cube)=>{
+    cubes.push(cube);
+    fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, " ", 4), {encoding: 'utf-8'})
+
+}
